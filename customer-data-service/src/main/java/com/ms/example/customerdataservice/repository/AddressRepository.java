d4e323a3a3ad694bf8bb85af3a63f4a0905c9ee0
avikdigidev <avikdigidev@gmail.com>
2022-11-02 00:24:26 +0530

    Updated Repositories

--- customer-data-service/src/main/java/com/ms/example/customerdataservice/repository/AddressRepository.java
+++ customer-data-service/src/main/java/com/ms/example/customerdataservice/repository/AddressRepository.java
@@ -1,7 +1,7 @@
 package com.ms.example.customerdataservice.repository;
 
 
-import com.ms.example.customer.portal.models.*;
+import com.ms.example.customerdataservice.model.*;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
 
--- customer-data-service/src/main/java/com/ms/example/customerdataservice/repository/CustomerRepository.java
+++ customer-data-service/src/main/java/com/ms/example/customerdataservice/repository/CustomerRepository.java
@@ -1,6 +1,6 @@
 package com.ms.example.customerdataservice.repository;
 
-import com.ms.example.customer.portal.models.*;
+import com.ms.example.customerdataservice.model.*;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
 
