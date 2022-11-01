d78d6c643cd3b713ec8d9b066bc1b950e9b2ea39
avikdigidev <avikdigidev@gmail.com>
2022-11-02 00:26:43 +0530

    Added class to test the functionality

---
+++ customer-data-service/src/main/java/com/ms/example/customerdataservice/service/DroolsConnectorService.java
@@ -0,0 +1,22 @@
+package com.ms.example.customerdataservice.service;
+
+import com.ms.example.customer.portal.models.dto.response.*;
+import com.ms.example.customer.portal.models.rulesModel.*;
+import org.kie.api.runtime.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DroolsConnectorService {
+//TODO: Refactor class properly
+    @Autowired
+    private KieSession session;
+@PostMapping("/customers")
+    public CustomerDT orderNow(CustomerDT customerDT) {
+        session.insert(customerDT);
+        session.fireAllRules();
+
+        return customerDT;
+    }
+}
